4995252e51f25133379c2eff78fb38f694364275
shatakshi-tech <gaurish305@gmail.com>
2026-03-07 02:26:41 +0530

    Initial Spring Boot project created

---
+++ src/main/java/com/example/signupform/SignupformApplication.java
@@ -0,0 +1,13 @@
+package com.example.signupform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signupform/SignupformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signupform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
